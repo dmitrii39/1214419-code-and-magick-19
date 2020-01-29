567e7e3515b937de58e025092c5435f3b3a8c544
dmitrii39 <d.tsyplov@mail.ru>
2020-01-29 21:21:33 +0200

    массив и глаза магов

--- js/setup.js
+++ js/setup.js
@@ -23,41 +23,66 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template')
 var wizards = [
 {
     name: WIZARD_NAMES[0],
+    surname: 'да Марья',
     coatColor: 'rgb(241, 43, 107)',
     eyesColor: 'red'
 },
 {
     name: WIZARD_NAMES[1],
+    surname: 'Верон',
     coatColor: 'rgb(215, 210, 55)',
     eyesColor: 'blue'
 },
 {
     name: WIZARD_NAMES[2],
+    surname: 'Мирабелла',
     coatColor: 'rgb(101, 137, 164)',
     eyesColor: 'yellow'
 },
 {
     name: WIZARD_NAMES[3],
+    surname: 'Вальц',
     coatColor: 'rgb(127, 127, 127)',
     eyesColor: 'green'
- }
+ },
+
+{
+    name: WIZARD_NAMES[4],
+    surname: 'Онопко',
+    coatColor: 'rgb (215, 210, 55)',
+    eyesColor: 'black'
+},
+
+{
+    name: WIZARD_NAMES[5],
+    surname: 'Топольницкая',
+    coatColor: 'rgb (0, 0, 0)',
+        
+},
+
+{
+    name: WIZARD_NAMES[6],
+    surname: 'Нионго'
+        
+},
+
+{
+    name: WIZARD_NAMES[7],
+    surname: 'Ирвинг'     
+}
+
 ];
 
-// var eyesColors = [
-// { eyesColor: 'black'},
-// { eyesColor: 'red'},
-// { eyesColor: 'blue'},
-// { eyesColor: 'yellow'},
-// { eyesColor: 'green'}
-// ];
 
-for (var i = 0; i < WIZARD_NAMES.length; i++) {
+for (var i = 0; i < 4; i++) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
-  wizardElement.querySelector('.setup-similar-label').textContent = wizards[i].name;
+wizardElement.querySelector('.setup-similar-label').textContent = wizards[i].name + ' '+ wizards[i].surname;
   wizardElement.querySelector('.wizard-coat').style.fill = wizards[i].coatColor;
   wizardElement.querySelector('.wizard-eyes').style.fill = wizards[i].eyesColor;
 
   similarListElement.appendChild(wizardElement);
 }
+
+
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
